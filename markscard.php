d201ba1a5bb223c70db8e8bf5771b75dbf24ad31
amreen235 <amreen2357@gmail.com>
2024-05-27 17:50:17 +0530

    merge

--- markscard.php
+++ markscard.php
@@ -50,6 +50,7 @@ if ($result->num_rows > 0) {
         echo "<td>" . $row["name"] . "</td>";
         echo "<td>" . $row["subject"] . "</td>";
         echo "<td>" . $row["marks"] . "</td>";
+        echo "<td>"
         echo "</tr>";
     }
 
